51c2fb47a643ad4b4e66f231d9159fd978a5f04d
carloscabello <_@carloscabello.net>
2021-12-02 17:51:59 +0100

    Removed unnecessary imports

--- aoc2021/utils.py
+++ aoc2021/utils.py
@@ -1,12 +1,6 @@
 """Basic input reader""" 
  
-import sys, os, inspect
-
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
-
-import utils
+import sys
 
 
 def to_int(strlist):
